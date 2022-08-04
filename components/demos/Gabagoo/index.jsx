a6e8bde3719b36137513fcbdfd43ad6c5895ba66
abstractigakis <ericstratigakis@gmail.com>
2022-08-04 19:55:40 -0400

    betterloader

--- components/demos/Gabagoo/index.jsx
+++ components/demos/Gabagoo/index.jsx
@@ -1,16 +1,11 @@
 import { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import { useRef } from "react";
 import Joey from "./Joey";
 
 const Gabagoo = () => {
-  const orbitControlsRef = useRef(null);
-
   return (
     <Canvas>
-      <Suspense fallback={null}>
-        <OrbitControls ref={orbitControlsRef} />
+      <Suspense fallback={<p>loading...</p>}>
         <ambientLight intensity={0.3} />
         <Joey />
       </Suspense>
