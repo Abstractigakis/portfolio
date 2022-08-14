5ff89280ea4f21419f37c2fb59eaa572802261b2
abstractigakis <ericstratigakis@gmail.com>
2022-08-14 10:59:21 -0400

    x

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,5 @@
 const Blog = () => {
-  return (
-    <iframe
-      src="https://publish.obsidian.md/abstractigakis"
-      className="h-screen w-screen"
-    />
-  );
+  return <iframe src="https://youtube.com" className="h-screen w-screen" />;
 };
 
 export default Blog;
