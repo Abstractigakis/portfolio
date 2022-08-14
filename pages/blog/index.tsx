6ceee5bea6bbe5e204ca8b11fb2772c331a9e484
abstractigakis <ericstratigakis@gmail.com>
2022-08-14 13:23:13 -0400

    z

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,7 +1,7 @@
 const Blog = () => {
   return (
     <iframe
-      src="https://publish.obsidian.md/abstractigakis"
+      src="https://publish.obsidian.md/abstractigakis/"
       className="h-screen w-screen"
     />
   );
